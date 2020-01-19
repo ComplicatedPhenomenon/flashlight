21310da2032deefe472aa70cb3b018f2014a00d2
ComplicatedPhenomenon <czfzdxx@outlook.com>
2020-01-19 15:10:21 +0800

    successfully send first request

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-extern crate reqwest;
-
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let body = reqwest::get("https://www.rust-lang.org")
         .await?
         .text()
         .await?;
 
     println!("body = {:?}", body);
+    Ok(())
 }
