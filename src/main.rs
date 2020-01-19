9dffd689502230ea3025f9e331494938b29977c1
ComplicatedPhenomenon <czfzdxx@outlook.com>
2020-01-19 19:50:04 +0800

    write to file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+use std::fs::File;
+use std::io::prelude::*;
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let body = reqwest::get("https://www.rust-lang.org")
+    let mut body = reqwest::get("https://github.com/timbl")
         .await?
         .text()
         .await?;
 
     println!("body = {:?}", body);
+    let mut file = File::create("timbl.html")?;
+    file.write(body.as_bytes())?;
+
     Ok(())
 }
